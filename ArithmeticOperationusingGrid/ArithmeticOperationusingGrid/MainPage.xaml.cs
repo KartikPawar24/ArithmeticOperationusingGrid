2495a0bb41505647d6fc57bed964e593b29ce101
Kartik Pawar <kartikmpawar.athani@hotmail.com>
2018-07-06 11:37:34 +0530

    In repo we will discuss how to create an simple calculator aaplication using Xamarin with Grid Layout.

--- ArithmeticOperationusingGrid/ArithmeticOperationusingGrid/MainPage.xaml.cs
+++ ArithmeticOperationusingGrid/ArithmeticOperationusingGrid/MainPage.xaml.cs
@@ -13,5 +13,46 @@ namespace ArithmeticOperationusingGrid
 		{
 			InitializeComponent();
 		}
-	}
+        private void BtnAdd_Clicked(object sender,EventArgs e)
+        {
+            double a =Convert.ToDouble(num1.Text);
+            double b = Convert.ToDouble(num2.Text);
+            res.Text = (a + b).ToString();
+        }
+
+        private void BtnSub_Clicked(object sender,EventArgs e)
+        {
+            double a = Convert.ToDouble(num1.Text);
+            double b = Convert.ToDouble(num2.Text);
+            res.Text = (a - b).ToString();
+        }
+
+        private void BtnMul_Clicked(object sender, EventArgs e)
+        {
+            double a = Convert.ToDouble(num1.Text);
+            double b = Convert.ToDouble(num2.Text);
+            res.Text = (a * b).ToString();
+        }
+
+        private void BtnDiv_Clicked(object sender, EventArgs e)
+        {
+            double a = Convert.ToDouble(num1.Text);
+            double b = Convert.ToDouble(num2.Text);
+            res.Text = (a / b).ToString();
+        }
+
+        private void BtnRem_Clicked(object sender, EventArgs e)
+        {
+            double a = Convert.ToDouble(num1.Text);
+            double b = Convert.ToDouble(num2.Text);
+            res.Text = (a % b).ToString();
+        }
+
+        private void BtnClr_Clicked(object sender, EventArgs e)
+        {
+            num1.Text = String.Empty;
+            num2.Text = String.Empty;
+            res.Text = "Result is:";
+        }
+    }
 }
